711a7e25d013674e89cc3c5a64451aa860a2f1dc
cedlau <cedric@mango3d.io>
2022-12-02 13:43:41 +0100

    new code

---
+++ src/utils.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void PrintText(char* txt)
+{
+    printf("%s\n", txt);
+}
+
---
+++ src/utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+extern void PrintText(char* txt);
+
+#endif // UTILS_H
