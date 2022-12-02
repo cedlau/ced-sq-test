a1bfe3bc792ec0cf686662e86c15043c5d076257
cedlau <cedric@mango3d.io>
2022-12-02 14:03:24 +0100

    new code

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 void PrintText(char* txt)
 {
-    printf("%s\n", txt);
+    if (strlen(txt) > 0)
+        if (txt[0] == '#')
+            printf("commment\n");
+        else
+            printf("%s\n", txt);
 }
 
--- test/tu_buggies.cpp
+++ test/tu_buggies.cpp
@@ -1,8 +1,14 @@
 #include <gtest/gtest.h>
 
 #include "../src/buggies.h"
+#include "../src/utils.h"
 
 TEST(TEST_buggies, init) {
     Buggy* buggy = new Buggy();
     EXPECT_TRUE(buggy != 0);
+}
+
+
+TEST(TEST_utils, PrintText_Comment) {
+    PrintText("#coucou");
 }
