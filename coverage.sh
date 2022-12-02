b7ee0db528d2dd6dd5d7165dab5eae4d78b3c20e
cedlau <cedric@mango3d.io>
2022-12-02 13:29:56 +0100

    analyze the dynamic library

--- coverage.sh
+++ coverage.sh
@@ -24,12 +24,11 @@ make
 rm -f *profraw
 rm -f *gcov
 rm -f *profdata
-CXX_EXECUTABLE="./test/tu_buggies"
-LLVM_PROFILE_FILE="code-%p.profraw" ${CXX_EXECUTABLE}
+CXX_EXECUTABLE="./src/libbuggy.so"
+LLVM_PROFILE_FILE="code-%p.profraw" ./test/tu_buggies
 
 llvm-profdata merge -output=code.profdata code-*.profraw
 llvm-cov report ${CXX_EXECUTABLE} -instr-profile=code.profdata -use-color -ignore-filename-regex=src/vendors/*
-#llvm-cov show --instr-profile merged.profdata build/coverage_test1 -object build/coverage_test2 > coverage.txt
-llvm-cov show ${CXX_EXECUTABLE} -instr-profile=code.profdata test/*.cpp > coverage.txt
+llvm-cov show ${CXX_EXECUTABLE} -instr-profile=code.profdata src/*.cpp > coverage.txt
 #llvm-cov show ${CXX_EXECUTABLE} -instr-profile=code.profdata src/*.cpp -path-equivalence -use-color --format html > coverage.html
 #echo "open coverage.html for HTML version of this report"
